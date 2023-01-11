790b80b3f486bbc77c1f85f1b16be4ffeda1191e
Blowar Aznable <wasuphonbeam@gmail.com>
2023-01-11 15:55:01 +0700

    Add project files.

---
+++ LprReportDemoV1/Form.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace LprReportDemoV1
+{
+  partial class Form
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.exportButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imgPathTextBox = new System.Windows.Forms.TextBox();
+            this.exportPathTextBox = new System.Windows.Forms.TextBox();
+            this.exportFileNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(15, 223);
+            this.exportButton.Margin = new System.Windows.Forms.Padding(6);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(563, 40);
+            this.exportButton.TabIndex = 0;
+            this.exportButton.Text = "Export";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.onExportButtonClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Image Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Export FilePath";
+            // 
+            // imgPathTextBox
+            // 
+            this.imgPathTextBox.Location = new System.Drawing.Point(185, 32);
+            this.imgPathTextBox.Name = "imgPathTextBox";
+            this.imgPathTextBox.Size = new System.Drawing.Size(393, 29);
+            this.imgPathTextBox.TabIndex = 3;
+            // 
+            // exportPathTextBox
+            // 
+            this.exportPathTextBox.Location = new System.Drawing.Point(185, 94);
+            this.exportPathTextBox.Name = "exportPathTextBox";
+            this.exportPathTextBox.Size = new System.Drawing.Size(393, 29);
+            this.exportPathTextBox.TabIndex = 4;
+            // 
+            // exportFileNameTextBox
+            // 
+            this.exportFileNameTextBox.Location = new System.Drawing.Point(185, 160);
+            this.exportFileNameTextBox.Name = "exportFileNameTextBox";
+            this.exportFileNameTextBox.Size = new System.Drawing.Size(220, 29);
+            this.exportFileNameTextBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "File Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(405, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = ".xls";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 278);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.exportFileNameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.exportPathTextBox);
+            this.Controls.Add(this.imgPathTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exportButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button exportButton;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox imgPathTextBox;
+    private System.Windows.Forms.TextBox exportPathTextBox;
+        private System.Windows.Forms.TextBox exportFileNameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ LprReportDemoV1/Form.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Xls;
+using CIT.LPR.SaveImageUtils;
+using CIT.Utils.Logs;
+
+
+
+namespace LprReportDemoV1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+
+        static CreativeLoggerV1 logger = new CreativeLoggerV1("LprReportDemo", @".\Logs\LprReportDemo");
+        LogFunctionV1 log = null;
+
+
+        public Form()
+        {
+            InitializeComponent();
+            this.log = logger.getLogFunctionV1();
+            this.log.logDebugCallback = (str) =>
+            {
+                Debug.WriteLine(str);
+                logger.logDebug(str);
+            };
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            exportFileNameTextBox.Text = $"LPR Report";
+            exportPathTextBox.Text = $"C:\\Users\\{Environment.UserName}\\Documents";
+        }
+
+
+        private void onExportButtonClicked(object sender, EventArgs eventArgs)
+        {
+
+            Exception exceptionThrown = null;
+            String baseImgPath = imgPathTextBox.Text;
+
+            if (String.IsNullOrWhiteSpace(baseImgPath))
+            {
+                MessageBox.Show("กรุณาใส่ Image Path", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            this.exportButton.Enabled = false;
+            Task.Run(() =>
+            {
+
+                SaveImageDirectoryInfo saveImageDirectory = new SaveImageDirectoryInfo(baseImgPath);
+                var saveImgFileInfoList = saveImageDirectory.getSaveImageFileInfo();
+
+                try
+                {
+                    Workbook workbook = new Workbook();
+                    Worksheet worksheet = workbook.Worksheets[0];
+
+                    #region    INIT_COLUMN_WIDTH
+
+                    worksheet.Range["A1"].ColumnWidth = 8;
+                    worksheet.Range["B1"].ColumnWidth = 60.25;
+                    worksheet.Range["C1"].ColumnWidth = 8.5;
+                    worksheet.Range["D1"].ColumnWidth = 8;
+
+                    #endregion INIT_COLUMN_WIDTH
+
+                    worksheet.Range["A1"].RowHeight = 20;
+
+                    worksheet.Range["A1"].Text = "Date";
+                    worksheet.Range["B1"].Text = "Picture";
+                    worksheet.Range["C1"].Text = "License";
+                    worksheet.Range["D1"].Text = "หมายเหตุ";
+
+                    worksheet.Range["A1"].Style.VerticalAlignment = VerticalAlignType.Center;
+                    worksheet.Range["B1"].Style.VerticalAlignment = VerticalAlignType.Center;
+                    worksheet.Range["C1"].Style.VerticalAlignment = VerticalAlignType.Center;
+                    worksheet.Range["D1"].Style.VerticalAlignment = VerticalAlignType.Center;
+
+
+                    for (int i = 0; i < saveImgFileInfoList.Count; i++)
+                    {
+                        int rowIndex = 2 + i;
+                        var eachSaveImgFileInfo = saveImgFileInfoList[i];
+                        String dateTimeStr = eachSaveImgFileInfo.saveImageDirectoryInfo.getDateStringForDisplayInUi() + "\n" + eachSaveImgFileInfo.getTimeStringForDisplayInUi();
+                        Image image = loadImage(eachSaveImgFileInfo.fullPath); //ดึงรูปมาใส่ตัวแปร image
+                        String licenseStr = eachSaveImgFileInfo.licensePlate; //ดึงทะเบียนมาใส่ตัวแปร licenseStr
+                        String province = eachSaveImgFileInfo.province; //ดึงจังหวัดมาใส่ตัวแปร province
+
+                        #region    INIT_ROW_WIDTH
+                        worksheet.Range["A" + rowIndex].RowHeight = 190;
+                        #endregion INIT_ROW_WIDTH
+
+                        //Add data to table
+                        worksheet.Range["A" + rowIndex].Text = dateTimeStr;
+                        worksheet.Pictures.Add(rowIndex, 2, image);
+                        worksheet.Range["C" + rowIndex].Text = licenseStr;
+                        worksheet.Range["D" + rowIndex].Text = "";
+
+                        //Allign output to center of the field
+                        worksheet.Range["A" + rowIndex].Style.VerticalAlignment = VerticalAlignType.Center;
+                        worksheet.Range["C" + rowIndex].Style.VerticalAlignment = VerticalAlignType.Center;
+                        worksheet.Range["C" + rowIndex].Style.HorizontalAlignment = HorizontalAlignType.Center;
+
+                        Thread.Sleep(100);
+                    }
+
+                    string borderBound = $"A1:D{(saveImgFileInfoList.Count + 1).ToString()}";
+                    CellRange borderRange = worksheet.Range[borderBound];
+                    borderRange.BorderAround(LineStyleType.Thin, Color.Black);
+                    borderRange.BorderInside(LineStyleType.Thin, Color.Black);
+
+                    string smallFontBound = $"A2:A{(saveImgFileInfoList.Count + 1).ToString()}";
+                    CellRange fontRange = worksheet.Range[smallFontBound];
+                    fontRange.Style.Font.Size = 8;
+
+                    workbook.SaveToFile(exportPathTextBox.Text + "\\" + exportFileNameTextBox.Text + ".xls");
+
+                }
+
+                catch (Exception exception)
+                {
+                    logger.logDebug("Error creating Excel : " + exception.Message);
+                    exceptionThrown = exception;
+                }
+
+                this.Invoke(new Action(() =>
+                {
+                    if (exceptionThrown != null) { MessageBox.Show("Error : " + exceptionThrown.Message); }
+                    this.exportButton.Enabled = true;
+                }));
+            });
+
+            //หา Library ที่สามารถต่อไฟล์ได้ + หรือรวมไฟล์ได้ *** 
+
+        }
+
+
+        #region RESIZE_IMAGE_UTILS
+
+        //public static Image resizeImage(Image imgToResize, Size size){
+        //  return (Image)(new Bitmap(imgToResize, size));
+        //}
+
+
+        public static Image loadImage(String imgPath)
+        {
+            Image baseImage = Image.FromFile(imgPath);
+            Image resizedImage = resizeImage(baseImage, new Size(16 * 28, 9 * 28));
+            baseImage.Dispose();
+            return resizedImage;
+        }
+
+        private static System.Drawing.Image resizeImage(System.Drawing.Image imgToResize, Size size)
+        {
+
+            //Get the image current width  
+            int sourceWidth = imgToResize.Width;
+            //Get the image current height  
+            int sourceHeight = imgToResize.Height;
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+            //Calulate  width with new desired size  
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            //Calculate height with new desired size  
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+            //New Width  
+            int destWidth = (int)(sourceWidth * nPercent);
+            //New Height  
+            int destHeight = (int)(sourceHeight * nPercent);
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            // Draw image with new width and height  
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+            return (System.Drawing.Image)b;
+        }
+
+        #endregion RESIZE_IMAGE_UTILS
+
+    }
+}
+
+
+/*
+Image ratio
+1920 / 1080
+
+192 / 108
+48 / 27
+16 / 9
+640 / 360 ( x 40 ) 
+
+ */
